a6b9b1df3a90d2c20727b3254ef75ab7a711fa41
David <Therobodavo@gmail.com>
2018-04-09 09:59:27 -0400

    added a player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-.21f,0),ForceMode2D.Impulse);
+        }
+	}
+}
