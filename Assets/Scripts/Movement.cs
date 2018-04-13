0d41eb77cf351d3aafe4e01432a0ad7d131b3be3
Tristan Marshall <tam8217@g.rit.edu>
2018-04-12 21:42:03 -0400

    Changed jump collision detection

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,20 +6,36 @@ public class Movement : MonoBehaviour {
 
     bool hasJumped = false;
     public Collider2D mainCol;
+    //List<Collider2D> jumpCols = new List<Collider2D>();
+    GameObject[] jumpColObjs;
+    List<Collider2D> jumpCols = new List<Collider2D>();
 	// Use this for initialization
 	void Start ()
     {
-		
+        //Finding the platforms to check for jumping
+        jumpColObjs = GameObject.FindGameObjectsWithTag("JumpCheck");
+        for (int i = 0; i < jumpColObjs.Length; i++)
+        {
+            jumpCols.Add(jumpColObjs[i].GetComponent<Collider2D>());
+        }
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         //gameObject.GetComponent<Rigidbody2D>().IsTouching()
-        if(gameObject.GetComponent<Rigidbody2D>().IsTouching(mainCol))
+        for (int i = 0; i < jumpColObjs.Length; i++)
+        {
+            if (gameObject.GetComponent<Rigidbody2D>().IsTouching(jumpCols[i]))
+            {
+                hasJumped = false;
+            }
+        }
+        /*if(gameObject.GetComponent<Rigidbody2D>().IsTouching(mainCol))
         {
             hasJumped = false;
         }
+        */
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(.21f,0),ForceMode2D.Impulse);
@@ -30,7 +46,7 @@ public class Movement : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.Space) && hasJumped == false)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 2.8f), ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 3.8f), ForceMode2D.Impulse);
             hasJumped = true;
         }
     }
