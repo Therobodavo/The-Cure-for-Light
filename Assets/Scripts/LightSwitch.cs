0bf95d624e3491cf2282be18c85debf73523f400
Tristan Marshall <tam8217@g.rit.edu>
2018-04-26 21:43:46 -0400

    Added Lightswitch classes

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public Light mainLight;
+    GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (mainLight.enabled == true)
+            {
+                mainLight.enabled = false;
+            }
+        }
+	}
+}
